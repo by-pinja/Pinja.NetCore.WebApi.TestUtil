e841a8894c5be1ce2136620634075b59047beb46
Pekka Savolainen <pekka.savolainen@pinja.com>
2026-03-09 18:14:22 +0200

    drop super old net core 2.1 support...

--- Pinja.NetCore.WebApi.TestUtil.Tests/TestStartup.cs
+++ Pinja.NetCore.WebApi.TestUtil.Tests/TestStartup.cs
@@ -13,30 +13,23 @@ namespace Pinja.NetCore.WebApi.TestUtil.Tests
 
         public void ConfigureServices(IServiceCollection services)
         {
-#if NETCOREAPP2_1
-            services.AddMvc();
-#else
+
             services.AddControllers()
                 .AddJsonOptions(options =>
                 {
                     options.JsonSerializerOptions.Converters.Add(new CustomTestObjectConverter());
                 });
-#endif
-            
+                
 
             services.AddSingleton(Substitute.For<IExternalDepency>());
         }
 
         public void Configure(IApplicationBuilder app)
         {
-#if NETCOREAPP2_1
-            app.UseMvc();
-#else
             app.UseRouting();
             app.UseEndpoints(endpoints => {
                 endpoints.MapControllers();
             });
-#endif
         }
     }
 }
